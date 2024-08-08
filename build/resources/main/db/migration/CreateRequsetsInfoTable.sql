5352c6ced71b68ffb288a92863ca0b0cc332b548
hallisca <hallisca@student.21-school.ru>
2024-08-08 19:49:58 +0700

    db properies

--- build/resources/main/db/migration/CreateRequsetsInfoTable.sql
+++ build/resources/main/db/migration/CreateRequsetsInfoTable.sql
@@ -1,14 +1,14 @@
 create table translates(
-    id integer PRIMARY KEY AUTO_INCREMENT,
-    ip_id varchar not null references requests_info(ip),
-    input_text varchar not null,
-    output_text varchar not null,
-    input_language varchar not null,
-    output_language varchar not null
-)
+   id integer PRIMARY KEY auto_increment,
+   ip_id varchar not null references requests_info(ip),
+   input_text varchar not null,
+   output_text varchar not null,
+   input_language varchar not null,
+   output_language varchar not null
+);
 
 create table requests_info(
-    id integer not null,
-    ip varchar(15) not null,
+    id integer not null AUTO_INCREMENT,
+    ip varchar(15) not null unique ,
     primary key (id),
 );
